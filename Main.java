fb9e2ecf55dfda3dea56ca040f0a357b1335ce21
Steven <stevccru@icloud.com>
2022-01-19 15:48:33 -0500

    working to implement try/catch

--- Main.java
+++ Main.java
@@ -1,35 +1,63 @@
 import java.util.Scanner;
 
  class Main {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
 
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("You are in a land full of dragons. In front of you, you see two caves. In one cave, the dragon is friendly and will share his treasure with you.\nThe other dragon is greedy and hungry and will eat you on sight. Which cave will you go into; cave1 or cave2?");
-
-        var input = myObj.next();
+         //Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
 
-        if ( input.equals ("cave1")) {
-            System.out.println("You approach the cave...It is dark and spooky...A large dragon jumps out in front of you! He opens his jaw and gobbles you in one bite!");
+         try {
+             Scanner myObj = new Scanner (System.in);
+             System.out.println("You are in a land full of dragons. In front of you, you see two caves. In one cave, the dragon is friendly and will share his treasure with you.\nThe other dragon is greedy and hungry and will eat you on sight. Which cave will you go into; cave1 or cave2?");
+             var input = myObj.next();
+             if (input.equals("cave1")) {
+                 System.out.println("You approach the cave...It is dark and spooky...A large dragon jumps out in front of you! He opens his jaw and gobbles you in one bite!");
+                 //return "Loser";
+             } else if (input.equals("cave2")) {
+                 System.out.println("You've met the friendly dragon! Party time!!!");
+                 //return "Winner";
+             }
+             //I want this to catch an error when the user inputs a value other than cave1 or cave2.
+         }catch (Exception e) {
+             System.out.printf("Please input exactly cave1 or cave2; any other input will not be accepted (input is case sensitive).");
+             //return "Please input exactly cave1 or cave2; any other input will not be accepted (input is case sensitive).";
 
-        }
-        //}else if (str == "cave2")
-           // return "You've met the friendly dragon!" ;
+         }
+        // return null;
+         }}
+         // Below printing out a string to tell the user what to do
+        /* System.out.println("You are in a land full of dragons. In front of you, you see two caves. In one cave, the dragon is friendly and will share his treasure with you.\nThe other dragon is greedy and hungry and will eat you on sight. Which cave will you go into; cave1 or cave2?");
 
-   // else return null;
+         //variable for user input
+         var input = myObj.next();
 
+         //setting first conditional for cave1
+         if (input.equals("cave1")) {
+             System.out.println("You approach the cave...It is dark and spooky...A large dragon jumps out in front of you! He opens his jaw and gobbles you in one bite!");
 
-
-
-
-
-
-    }
-
-
-}
-
+         } else if (input.equals("cave2")) {
+             System.out.println("You've met the friendly dragon! Party time!!!");
 
 
 
 
+         }
+
+         try{
+             return (input = "cave1" || "cave2");
+
+         }catch (Exception e){
+
+             return "Caught Exception: User input must be String of cave1 or cave2";
+
+         }
+
+
+     }
+ }
+
+
+
+
+
+*/
