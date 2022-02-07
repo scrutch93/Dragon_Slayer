341a997261fdce8f9dabc22e96a75960202ca5d1
Steven <stevccru@icloud.com>
2022-02-07 13:36:51 -0500

    successfully added try/catches

--- Main.java
+++ Main.java
@@ -6,6 +6,13 @@ import java.util.Scanner;
          //Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 
 
+         //return "Please input exactly cave1 or cave2; any other input will not be accepted (input is case sensitive).";
+        startGame();
+     }
+     // return null;
+
+
+      static void startGame() {
          try {
              Scanner myObj = new Scanner (System.in);
              System.out.println("You are in a land full of dragons. In front of you, you see two caves. In one cave, the dragon is friendly and will share his treasure with you.\nThe other dragon is greedy and hungry and will eat you on sight. Which cave will you go into; cave1 or cave2?");
@@ -16,15 +23,20 @@ import java.util.Scanner;
              } else if (input.equals("cave2")) {
                  System.out.println("You've met the friendly dragon! Party time!!!");
                  //return "Winner";
-             }
+             }else if (input != "cave1" && input != "cave2") throw new Exception ("Please input exactly cave1 or cave2; any other input will not be accepted (input is case sensitive)");
+
+
              //I want this to catch an error when the user inputs a value other than cave1 or cave2.
          }catch (Exception e) {
+
+
              System.out.printf("Please input exactly cave1 or cave2; any other input will not be accepted (input is case sensitive).");
-             //return "Please input exactly cave1 or cave2; any other input will not be accepted (input is case sensitive).";
+             startGame();
+
 
          }
-        // return null;
-         }}
+     }
+ }
          // Below printing out a string to tell the user what to do
         /* System.out.println("You are in a land full of dragons. In front of you, you see two caves. In one cave, the dragon is friendly and will share his treasure with you.\nThe other dragon is greedy and hungry and will eat you on sight. Which cave will you go into; cave1 or cave2?");
 
